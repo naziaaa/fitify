6d72d4f70cf1faa00836f1d7efed821e0a02b478
name <tnazia377@gmail.com>
2022-10-08 15:37:40 +0530

    global constants added

---
+++ lib/core/const/global_constants.dart
@@ -0,0 +1,7 @@
+import 'package:fitness/data/user_data.dart';
+import 'package:fitness/data/workout_data.dart';
+
+class GlobalConstants{
+  static late UserData currentUser;
+  static late List<WorkoutData> workouts;
+}
